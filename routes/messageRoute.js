ce2f8e977e4f1193e596ca2dd923158ecf97b425
shayanahmad1999 <shayanahmad235@gmail.com>
2023-11-01 22:44:42 -0700

    add routes to messageRoute

--- routes/messageRoute.js
+++ routes/messageRoute.js
@@ -5,6 +5,7 @@ const userController = require('./../controllers/userController');
 const router = express.Router();
 
 router.route('/').get(userController.protect, messageController.index);
+router.route('/create').get(userController.protect, messageController.createView);
 router.route('/create').post(userController.protect, messageController.create);
 
 module.exports = router;
