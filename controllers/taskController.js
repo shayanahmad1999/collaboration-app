59672a1178613c288011e79860e03f25292f93c6
shayanahmad1999 <shayanahmad235@gmail.com>
2023-11-02 02:11:17 -0700

    task crud complete

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -3,14 +3,11 @@ const asyncErrorHandler = require('../utils/asyncErrorHandler');
 const customeError = require('../utils/customeError');
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
 
 exports.index = async(req, res, next) => {
     
-    const testToken = req.headers.authorization;
-    let token;
-    if(testToken && testToken.startsWith('bearer')){
-        token = testToken.split(' ')[1];
-    }
+    const token = req.cookies.jwt;
     if(!token){
         next(new customeError('you are not logged in!', 401))
     }
@@ -20,14 +17,28 @@ exports.index = async(req, res, next) => {
 
     const tasks = await Task.find(
         {
-            $or: [{ sender_id: userId }, { receiver_id: userId }]
+            $or: [{ assigned_by: userId }, { assigned_to: userId }]
         }
     ).populate('assigned_to').populate('assigned_by');
-    res.json({
-        total: tasks.length, 
-        tasks
-    });
+     // for web page
+     const successMessage = [] ;
+     res.render('tasks/index', {successMessage, tasks: tasks, total: tasks.length, title: 'Tasks' });
+    // res.json({
+    //     total: tasks.length, 
+    //     tasks
+    // });
 }
+
+exports.createView = async (req, res, next) => {
+    const token = req.cookies.jwt;
+    const decoded = jwt.verify(token, process.env.SECRET_JWT_STR); 
+    const userId = decoded.id;
+    const user = await User.find({
+        $or: [{ _id: userId }]
+    });
+    const users = await User.find({});
+    res.render('tasks/create', {users: users, user: user, title: 'Create', user: req.user });
+};
 
 exports.create = asyncErrorHandler (async(req, res, next) => {
     const {task_name, description, assigned_to, assigned_by, due_date } = req.body;
@@ -46,3 +57,27 @@ exports.create = asyncErrorHandler (async(req, res, next) => {
         } 
     });
 });
+
+exports.details = (req, res, next) => {
+    const id = req.params.id;
+    Task.findById(id).populate('assigned_to').populate('assigned_by')
+    .then((result) => {
+        // for web page
+        res.render('tasks/details', {task: result, title: 'Task Details'});
+    })
+    .catch((err) => {
+        // for web page
+        res.status(404).render('404-page', {title: 'Task not found'});
+    });
+};
+
+exports.destroy = (req, res) => {
+    const id = req.params.id;
+    Task.findByIdAndDelete(id)
+    .then((result) => {
+        res.json({redirect: '/tasks'});
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+};
--- routes/taskRoute.js
+++ routes/taskRoute.js
@@ -5,6 +5,9 @@ const userController = require('./../controllers/userController');
 const router = express.Router();
 
 router.route('/').get(userController.protect, taskController.index);
+router.route('/create').get(userController.protect, taskController.createView);
 router.route('/create').post(userController.protect, taskController.create);
+router.route('/details/:id').get(userController.protect, taskController.details);
+router.route('/:id').delete(userController.protect, taskController.destroy);
 
 module.exports = router;
