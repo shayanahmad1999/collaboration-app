b35c4c91b355e047a2b1eba27eb066f9c6748e42
shayanahmad1999 <shayanahmad235@gmail.com>
2023-11-01 22:44:59 -0700

    some route add to userRoute

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,8 +4,12 @@ const userController = require('./../controllers/userController');
 const router = express.Router();
 
 router.route('/').get(userController.users);
+router.route('/login').get(userController.loginView);
+router.route('/signup').get(userController.signupView);
+
 router.route('/signup').post(userController.signup);
 router.route('/login').post(userController.login);
+router.route('/logout').get(userController.logout);
 router.route('/forgotPassword').post(userController.forgotPassword);
 router.route('/resetPassword/:token').post(userController.resetPassword);
 
